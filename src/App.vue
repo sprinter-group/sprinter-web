369b66ee67536abcb7bf8aeaf5b272dd5ccc9328
spemer <ghsspower@naver.com>
2018-03-24 17:14:05 +0900

    style

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,52 @@ export default {
 </script>
 
 <style lang="scss">
+@import url(https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
+
+// mixin default font
+@mixin font-stack-html() {
+    font-family: 'Spoqa Han Sans', 'Source Sans Pro', 'Apple SD Gothic Neo', 'Nanum Barun Gothic', 'Nanum Gothic', '돋움', 'Dotum', 'Verdana', 'Arial', 'sans-serif';
+    -webkit-font-family: 'Spoqa Han Sans', 'Source Sans Pro', 'Apple SD Gothic Neo', 'Nanum Barun Gothic', 'Nanum Gothic', '돋움', 'Dotum', 'Verdana', 'Arial', 'sans-serif';
+}
+
+// text rendering
+@mixin font-settings() {
+    direction: ltr;
+    font-feature-settings: 'liga';
+    -moz-font-feature-settings: 'liga';
+    text-rendering: optimizeLegibility;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -webkit-text-decoration-skip: ink;
+}
+
+// media query breakpoints
+$tablet: "only screen and (max-width : 768px)";
+$landsc: "only screen and (max-width : 600px)";
+$middle: "only screen and (max-width : 520px)";
+$mobile: "only screen and (max-width : 420px)";
+
+// default font-size, line-height
+@function calculateRem($size) {
+    $remSize: $size / 16px;
+    @return $remSize * 1rem;
+}
+@mixin font-size($size) {
+    font-size: $size;
+    font-size: calculateRem($size);
+}
+@mixin line-height($size) {
+    line-height: $size * 1.6;
+    line-height: calculateRem($size) * 1.6;
+}
+
+// opacity
+@mixin opacity($opacity) {
+    opacity: $opacity;
+    $opacity-ie: $opacity * 100;
+    filter: alpha(opacity=$opacity-ie);
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,6 +70,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+// fonts
+body {
+    @include font-size(16px);
+    @include line-height(16px);
+    @include font-stack-html();
+    @include font-settings();
 }
 
 h1, h2 {
